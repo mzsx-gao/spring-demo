f752eba393baf6ad741654bc9e665a7f70d68dcc
gaoshudian <18818196720@163.com>
2019-09-05 22:13:31 +0800

    小幅度改动

--- spring-beans/src/main/java/com/gao/spring/customer/CustomTagTest.java
+++ spring-beans/src/main/java/com/gao/spring/customer/CustomTagTest.java
@@ -2,11 +2,8 @@ package com.gao.spring.customer;
 
 
 import org.junit.Test;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.ClassPathResource;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
