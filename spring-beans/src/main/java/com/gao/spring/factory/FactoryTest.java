5dc15b4ebd51b8376f45adf24d746a7d8a3b63aa
gaoshudian <18818196720@163.com>
2019-09-08 22:56:05 +0800

    小幅度改动

--- spring-beans/src/main/java/com/gao/spring/factory/FactoryTest.java
+++ spring-beans/src/main/java/com/gao/spring/factory/FactoryTest.java
@@ -1,33 +1,39 @@
 package com.gao.spring.factory;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class FactoryTest {
 
-    ApplicationContext ac = new ClassPathXmlApplicationContext("classpath:factory.xml");
+    ApplicationContext ac = null;
+
+    @Before
+    public void before() {
+        ac = new ClassPathXmlApplicationContext("classpath:factory.xml");
+    }
 
     //静态工厂方法配置bean
     @Test
-    public void test1(){
-        Car car = (Car)ac.getBean("car");
+    public void test1() {
+        Car car = (Car) ac.getBean("car");
         System.out.println(car);
     }
 
     //实例工厂方法配置bean
     @Test
-    public void test2(){
-        Car car = (Car)ac.getBean("car2");
+    public void test2() {
+        Car car = (Car) ac.getBean("car2");
         System.out.println(car);
     }
 
     //factoryBean方式配置bean
     @Test
-    public void test3(){
+    public void test3() {
         User user = (User)ac.getBean("user");
         System.out.println(user);
-        UserBean userBean = (UserBean) ac.getBean("&user");
-        System.out.println(userBean.getObject());
+//        UserBean userBean = (UserBean) ac.getBean("&myUser");
+//        System.out.println(userBean.getObject());
     }
 }
